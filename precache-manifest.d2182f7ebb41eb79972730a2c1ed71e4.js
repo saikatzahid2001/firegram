e9ef49d73d9423a4d687a3374588f499fc34c3d8
saikatzahid2001 <saikat123321098@gmail.com>
2021-04-15 15:40:10 +0600

    Updates

---
+++ precache-manifest.d2182f7ebb41eb79972730a2c1ed71e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771f4de21ea3e7a59b01896ed7b20226",
+    "url": "/firegram/index.html"
+  },
+  {
+    "revision": "f0f5e1ae7510582f213f",
+    "url": "/firegram/static/css/main.7ece580b.chunk.css"
+  },
+  {
+    "revision": "bc9d04d33242e7367d42",
+    "url": "/firegram/static/js/2.60754ac6.chunk.js"
+  },
+  {
+    "revision": "f13b8ee62297cdeb55e53ad5d3f0bd7f",
+    "url": "/firegram/static/js/2.60754ac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0f5e1ae7510582f213f",
+    "url": "/firegram/static/js/main.5a5ffb29.chunk.js"
+  },
+  {
+    "revision": "a20b20e63cdcd0ca9e81",
+    "url": "/firegram/static/js/runtime-main.9968160c.js"
+  }
+]);
